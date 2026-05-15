b02ccc8e290fe289023256c5d8e39a00f1a3541a
tuyishime eric <erictuyishime2004@gmail.com>
2026-05-15 09:12:22 +0200

    the cluster module

---
+++ cluster.js
@@ -0,0 +1,20 @@
+const cluster = require("cluster");
+const os = require("os");
+const express = require("express");
+const app = express();
+
+const cpuCount = os.cpus.length;
+
+if (cluster.isMaster) {
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork;
+    cluster.fork;
+  }
+} else {
+  app.get("/", (req, res) => {
+    res.send(`you are connected to the ${process.pid}`);
+  });
+  app.listen(3000, () => {
+    console.log(`you are listening to ${process.pid}`);
+  });
+}
