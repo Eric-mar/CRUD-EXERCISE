6335cde3bb0af2ddbd2ab79b3686fd6d5f9fb5a7
tuyishime eric <erictuyishime2004@gmail.com>
2026-05-15 09:24:30 +0200

    fix: changing isMaster old version to isPrimary modern way

--- cluster.js
+++ cluster.js
@@ -3,18 +3,18 @@ const os = require("os");
 const express = require("express");
 const app = express();
 
-const cpuCount = os.cpus.length;
+const cpuCount = os.cpus().length;
 
-if (cluster.isMaster) {
+if (cluster.isPrimary) {
   for (let i = 0; i < cpuCount; i++) {
-    cluster.fork;
-    cluster.fork;
+    cluster.fork()
+    
   }
 } else {
   app.get("/", (req, res) => {
     res.send(`you are connected to the ${process.pid}`);
   });
-  app.listen(3000, () => {
+  app.listen(8000, () => {
     console.log(`you are listening to ${process.pid}`);
   });
 }
